91bc1c3be34c7af88b2e93ce009d093dc14538cb
yocosaka <sakayoco55@gmail.com>
2020-11-19 13:21:26 +0900

    Install rspec and Add spec file to test methods

--- my_quotes_bot.rb
+++ my_quotes_bot.rb
@@ -6,8 +6,18 @@ require 'chatterbot/dsl'
 # Call a custom file
 require_relative './lib/my_tweets.rb'
 
-mytweets = MyTweets.new
-tweet mytweets.pickup_quote + mytweets.pickup_emoji
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.access_token = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
+
+loop do
+  mytweets = MyTweets.new
+  client.update mytweets.pickup_quote + mytweets.pickup_emoji
+  sleep 3600
+end
 
 #
 # Hello! This is some starter code for your bot. You will need to keep
